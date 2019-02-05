6274eeb564214b9e070eaf2555e65407180a252b
Stephen Abello <stephen@apinc.org>
2019-02-05 10:58:29 +0100

    Update version de dev 0.1.3

--- module.itop-assign-to-me.php
+++ module.itop-assign-to-me.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'itop-assign-to-me/0.1.2',
+	'itop-assign-to-me/0.1.3-dev',
 	array(
 		// Identification
 		//
